10150bf3428d66c1df0cc463f4b8ea5d6129ea20
nguyenthdat <nguyenthdat@outlook.com>
2022-01-23 11:40:50 +0000

    update

--- lession03/main.go
+++ lession03/main.go
@@ -24,6 +24,18 @@ type Book struct {
 	PersonID   int
 }
 
+var (
+	person = &Person{
+		Name:  "Jason",
+		Email: "me@nguyenthdat.com",
+	}
+	books = []Book{
+		{Title: "a", Author: "b", CallNumber: 1, PersonID: 1},
+		{Title: "c", Author: "d", CallNumber: 2, PersonID: 1},
+		{Title: "e", Author: "f", CallNumber: 3, PersonID: 1},
+	}
+)
+
 func main() {
 	// Loading env
 	host := os.Getenv("HOST")
@@ -33,13 +45,19 @@ func main() {
 	dbport := os.Getenv("DBPORT")
 
 	//Connection string
-	dbURL := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s", host, user, dbname, password, dbport)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, dbport)
 
 	//Connect to database
-	_, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		fmt.Printf("Successfully connect to database")
+	}
+	db.AutoMigrate(&Person{})
+	db.AutoMigrate(&Book{})
+	db.Create(&person)
+	for i := range books {
+		db.Create(&books[i])
 	}
 }
