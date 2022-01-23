63810dcaeb68b71e253f3cad64592b24aa9415da
nguyenthdat <nguyenthdat@outlook.com>
2022-01-23 04:42:41 +0000

    update

--- lession01/main.go
+++ lession01/main.go
@@ -1,6 +1,22 @@
 // Lesson 1 - Binary Search, Linked Lists and Complexity
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+func main() {
+	slice := []int{13, 11, 12, 7, 4, 3, 1, 0}
+	fmt.Println(locateCard(slice, 7))
+}
+
+func locateCard(slice []int, index int) int {
+	sort.Sort(sort.Reverse(sort.IntSlice(slice))) // To sort in descending order, we use sort.Reverse function.
+	for i, v := range slice {
+		if v == index {
+			return i
+		}
+	}
+	return 0
 }
