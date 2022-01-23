041eebea37ed3ea9d60db739e69fbcd5ba5dd9c9
nguyenthdat <nguyenthdat@outlook.com>
2022-01-23 06:35:38 +0000

    update

--- lession02/main.go
+++ lession02/main.go
@@ -64,13 +64,30 @@ func (l *DoublyLinkedList) addNodeAfter(data string, key string) {
 			new_node.next = next_node
 			next_node.prev = &new_node
 			new_node.prev = current_node
+			return
 		}
 		current_node = current_node.next
 	}
 }
 
 func (l *DoublyLinkedList) addNodeBefore(data string, key string) {
-
+	current_node := l.head
+	var new_node Node
+	for current_node != nil {
+		if current_node.prev == nil && current_node.data == key {
+			l.prepend(data)
+			return
+		} else if current_node.data == key {
+			new_node.data = data
+			prev_node := current_node.prev
+			prev_node.next = &new_node
+			current_node.prev = &new_node
+			new_node.next = current_node
+			new_node.prev = prev_node
+			return
+		}
+		current_node = current_node.next
+	}
 }
 
 func (l *DoublyLinkedList) print_list() {
@@ -89,6 +106,8 @@ func main() {
 	dlst.append("c")
 	dlst.append("d")
 	dlst.prepend("e")
-	dlst.addNodeAfter("f", "b")
+	dlst.addNodeBefore("f", "b")
+	dlst.addNodeBefore("h", "e")
+	dlst.addNodeAfter("k", "d")
 	dlst.print_list()
 }
