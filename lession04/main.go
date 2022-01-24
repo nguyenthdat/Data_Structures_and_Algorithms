8a633c539bc46282310167597c94c4afb2f55007
nguyenthdat <nguyenthdat@outlook.com>
2022-01-24 09:46:56 +0000

    update

--- lession04/main.go
+++ lession04/main.go
@@ -1,3 +1,12 @@
+//Binary Trees in GoLang
+
+/*
+Binary Tree
+A binary tree is a tree data structure in which each node
+has at most two children, which are referred to as the left
+child and the right child.
+*/
+
 package main
 
 func main() {
